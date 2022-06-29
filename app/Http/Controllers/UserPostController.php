03575dad6259d7920df551965a76d6d6d1427175
ArtuRuschak <ruschakartur@gmail.com>
2022-06-29 15:56:33 +0300

    Add type hints and doc strings

--- app/Http/Controllers/UserPostController.php
+++ app/Http/Controllers/UserPostController.php
@@ -7,6 +7,9 @@ use Lucid\Units\Controller;
 
 class UserPostController extends Controller
 {
+    /**
+     * @return mixed
+     */
     public function index(){
         return $this->serve(UserPostIndexFeature::class);
     }
