2f03e31cfdc2e9fa55f42370cfdc7eeafe6a314e
ArtuRuschak <ruschakartur@gmail.com>
2022-07-06 14:34:48 +0300

    add "enter" to code style

--- app/Features/StorePostFeature.php
+++ app/Features/StorePostFeature.php
@@ -4,11 +4,15 @@ namespace App\Features;
 
 use App\Domains\Http\Jobs\RedirectBackJob;
 use App\Domains\Post\Jobs\SavePostJob;
+use App\Domains\Post\Jobs\SendPostMailMessageJob;
 use App\Domains\Post\Jobs\UploadPostImageJob;
 use App\Domains\Post\Requests\StorePost;
+use App\Domains\User\Jobs\GetUserListJob;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Arr;
 use Lucid\Units\Feature;
+use Ramsey\Collection\Collection;
 
 class StorePostFeature extends Feature
 {
@@ -25,9 +29,16 @@ class StorePostFeature extends Feature
             'data' => Arr::except($data, 'img'),
         ]);
 
+        /** @var Collection $users */
+        $users = $this->run(GetUserListJob::class);
+
         $this->run(UploadPostImageJob::class, [
             'image' => $request->file('img'),
-            'post' => $post
+            'post'  => $post
+        ]);
+        $this->run(SendPostMailMessageJob::class, [
+            'post'   => $post,
+            'emails' => $users->pluck('email')->toArray(),
         ]);
 
         return $this->run(RedirectBackJob::class, [
